bbb5d6ba5342928125178c2847cc441c4924b7b1
Naoya Ozaki <naoya.ozaki11@gmail.com>
2022-12-20 21:04:42 +0900

    Added two-body problem example

--- src/NBodyProblem.jl
+++ src/NBodyProblem.jl
@@ -1,7 +1,18 @@
 
 module NBodyProblem
+using LinearAlgebra
 
 include("SolarSystemDynamics.jl")
 # Write your package code here.
+
+export twobody!
+
+# Definition of Ordinary Differential Equation
+function twobody!(dxdt, x, p, t)
+    # dx/dt
+    r = norm(x[1:3])
+    dxdt[1:3] = x[4:6]
+    dxdt[4:6] = -ssd.GM["EARTH"] * x[1:3] / (r^3)
+end
 
 end
