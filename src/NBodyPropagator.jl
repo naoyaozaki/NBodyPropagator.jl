91c4b873f27edf7ed6daf7045bfb220c5dc62054
Naoya Ozaki <naoya.ozaki11@gmail.com>
2023-02-03 00:32:03 +0900

    ⚙Refactored

--- src/NBodyPropagator.jl
+++ src/NBodyPropagator.jl
@@ -139,14 +139,4 @@ function propagate(nbp::NBodyProblem; kwargs...)
     return sol
 end
 
-
-
-# Definition of Ordinary Differential Equation
-function twobody!(dxdt, x, p, t)
-    # dx/dt
-    r = norm(x[1:3])
-    dxdt[1:3] = x[4:6]
-    dxdt[4:6] = -ssd.GM["EARTH"] * x[1:3] / (r^3)
-end
-
 end
